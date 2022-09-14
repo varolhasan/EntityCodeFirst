3ab75caee63e0d416dc65e844678325f6aef7435
Hasan VAROL <hasanvarooll@gmail.com>
2022-09-14 03:08:29 +0300

    Add project files.

---
+++ EntityCodeFirst/Migrations/202209122054274_RenameKategoriisimColumnToKategoriAdiColumnInKategorisTable.cs
@@ -0,0 +1,22 @@
+﻿namespace EntityCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameKategoriisimColumnToKategoriAdiColumnInKategorisTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Kategoris", "KategoriAdi", c => c.String());
+            Sql("update Kategoris set KategoriAdi=KategoriAd");
+            DropColumn("dbo.Kategoris", "KategoriAd");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Kategoris", "KategoriAd", c => c.String());
+            Sql("update Kategoris set KategoriAd=KategoriAdi"); 
+            DropColumn("dbo.Kategoris", "KategoriAdi");
+        }
+    }
+}
---
+++ EntityCodeFirst/Migrations/202209122111164_AddKategoriDetayColumnInKategorisTable.cs
@@ -0,0 +1,18 @@
+﻿namespace EntityCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddKategoriDetayColumnInKategorisTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Kategoris", "KategoriDetay", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Kategoris", "KategoriDetay");
+        }
+    }
+}
